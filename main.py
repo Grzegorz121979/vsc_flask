0471672f15fc25e0a39a3ea4a275a58e6cb89a2c
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-03-14 16:22:26 +0100

    select/option element

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ def calculate_bmi():
         weight_input = float(weight)
         height_input = float(height)
         selected_option = request.form.get('radio_button')
-        result_men = round(weight_input / height_input ** 2, 2)
-        result_women = round(weight_input / height_input ** 2, 2)
+        result_men = round(weight_input / (height_input / 100) ** 2, 2)
+        result_women = round(weight_input / (height_input / 100) ** 2, 2)
         if selected_option == 'men_value':
             return render_template('index.html', result_men=result_men)
         elif selected_option == 'women_value':
             return render_template('index.html', result_women=result_women)
         else:
-            return 'Select field Man or Women'
+            error = 'Select field Man or Women'
+            return render_template('index.html', error=error)
     except ValueError:
         error = 'All position should be fill'
         return render_template('index.html', error=error)
