f4e52181af5cac71dd55582caa6f5207a1d5a35f
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-05-02 14:14:43 +0200

    Update save_data function

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+import csv
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def start():
+    with open('assets/data.txt', 'w', encoding='utf-8') as file:
+        file.write("")
     return render_template('index.html')
+
 
 @app.route('/submit', methods=['POST'])
 def calculate_bmi():
     try:
-        weight = request.form['weight_input']
-        height = request.form['height_input']
+        weight = request.form.get('weight_input')
+        height = request.form.get('height_input')
         weight_input = float(weight)
         height_input = float(height)
         selected_option = request.form.get('radio_button')
         result_man = round(weight_input / (height_input / 100) ** 2, 2)
         result_women = round(weight_input / (height_input / 100) ** 2, 2)
+
+        with open("assets/data.txt", "w", encoding="utf-8") as file:
+            file.write(weight + "\n")
+            file.write(str(result_man) + "\n")
+
         if selected_option == 'man_value':
             return render_template('index.html', result_man=result_man)
         elif selected_option == 'women_value':
@@ -26,15 +38,30 @@ def calculate_bmi():
     except ValueError:
         error = 'All position should be fill'
         return render_template('index.html', error=error)
-    
+
+
 @app.route('/save', methods=['POST'])
 def save_data():
-    weight = request.form.get('weight_input')
+    date = datetime.now().strftime("%d/%m/%Y")
+    with open("assets/data.txt", "r", encoding="utf-8") as file:
+        lst = [line.strip() for line in file.readlines()]
 
-    with open('assets\data.txt', 'a', encoding='utf-8') as file:
-        file.write(weight + '\n')
+    file_exists = os.path.exists("assets/output.csv")
+
+    with open("assets/output.csv", "a", newline="", encoding="utf-8") as csv_file:
+        header = ["Date", "Weight", "BMI"]
+        writer = csv.DictWriter(csv_file, fieldnames=header)
+
+        if not file_exists:
+            writer.writeheader()
+
+        writer.writerow({"Date": date, "Weight": (lst[0] + "kg"), "BMI": lst[1]})
+
+        with open('assets/data.txt', 'w', encoding='utf-8') as file:
+            file.write("")
     
-    return 'Data saved!'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
